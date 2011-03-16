dd311fd3dd0cdb0dabf91c8d93a97b5e73befad5
Alistair Stead <alistair.stead@designdisclosure.com>
2011-03-16 14:27:11 +0000

    Added a new modules recipe to copy application modules deployed seperatly to a larger project

---
+++ Module-Recipe.rb
@@ -0,0 +1,15 @@
+namespace :module do
+  desc <<-DESC
+    Copy the most recent release of the project into the destination directory. This allows you to
+    deploy new releases of a module and then copy them into place within a larger project. You should only use this
+    when git submodules or symlinks are not an option.
+      
+    #{module_from} The location in your codebase to be copied.
+    #{module_to} The location in the destination project the files should be copied to.
+  DESC
+  task :copy, :roles => :web, :except => { :no_release => true } do
+    run "#{try_sudo} cp -RPf #{latest_release}#{module_from} #{module_to}"
+  end
+end
+
+after   'deploy:finalize_update', 'module:copy'
